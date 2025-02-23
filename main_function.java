9a364359f1cd0bf49e6d700011f84d4d85528a24
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2025-02-23 16:09:32 +0530

    primitive variables

---
+++ main_function.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+        String s = "moto";
+        int a = 45;
+        char c = 'd';
+        boolean e = true;
+        System.out.println("Username :"+ a + c + e + s);
+    }
+}
