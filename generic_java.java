9b76afce566d58f594fe73b142c943ba1495857b
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2025-03-19 18:47:43 +0530

    generic method ,class with autoboxing and unboxing

--- generic_java.java
+++ generic_java.java
@@ -1,14 +1,35 @@
 public class generic_java{
     public static void main(String[] args){
 
-        add<Integer> obj = new add<>(34,23);
-        add<String> oj = new add<>("wazim","raju");
+        // Integer a = new Integer(23);
+        Integer b = Integer.valueOf(27);
+        Integer c = 15; // autoboxing
+        int d = c; // unboxing
+
+        // System.out.println(a + " " + a.getClass().getName());
+        System.out.println(b + " " + b.getClass().getName());
+        System.out.println(c + " "+ c.getClass().getName());
+        // System.out.println(d + " " + d.getClass().getName());
+
 
         
+        add<Integer> obj = new add<>(34,23);
+        add<String> oj = new add<>("wazim","raju");
+        
+        generic_java objj = new generic_java();
+        objj.gene(13);
+        
+
+
+        
+        
+    }
+    public<E extends Number> void gene(E data){
+        System.out.println(data);
     }
 }
 
-class add<T >{
+class add<T  >{
     T e;
     T d;
     
